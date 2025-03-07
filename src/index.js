8308b01ce3e084ce470ea93a23700fe992630d09
Tofer-Cich <[tofercich@hotmail.com]>
2025-03-07 12:18:03 -0600

    Todo add button now lets user create a todo, but doesn't save it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./styles.css";
-import { createProjectCard } from "./card";
+import { createProjectCard, createTodoCard } from "./card";
 
 const createProjectButton = document.querySelector("nav > button");
 createProjectButton.addEventListener('click', function() {
     createProjectCard();
+});
+
+const createTodoButton = document.querySelector("#todo-window > button");
+createTodoButton.addEventListener('click', function() {
+    createTodoCard();
 });
